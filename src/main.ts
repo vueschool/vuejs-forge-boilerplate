43526bf7b8969cc1de50d5876edd351a7e0cee65
Daniel Kelly <me@danielkelly.io>
2022-06-28 11:11:23 -0500

    install tailwind css

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@/assets/base.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
