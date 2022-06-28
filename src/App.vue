7e25bfe1e6f8fde09f565a5a5f80dc025629146f
Daniel Kelly <me@danielkelly.io>
2022-06-28 12:12:36 -0500

    branding

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <AppButton>Hello world</AppButton>
+  <div class="flex justify-center items-center h-[80vh]">
+    <div class="text-center">
+      <img
+        class="block"
+        src="https://vuejsforge.com/images/logo.svg"
+        alt="Vue.js Forge"
+      />
+      <AppButton class="block">
+        <a href="https://vi.to/hubs/vueforge" target="_blank">Get Started</a>
+      </AppButton>
+    </div>
+  </div>
 </template>
