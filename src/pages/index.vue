35c286bd58e895c19f13a2a19b419288c544d4a9
Daniel Kelly <me@danielkelly.io>
2022-06-28 12:18:59 -0500

    boilerplate file-based routing

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex justify-center items-center h-[80vh]">
+    <div class="text-center">
+      <img
+        class="block"
+        src="https://vuejsforge.com/images/logo.svg"
+        alt="Vue.js Forge"
+      />
+      <AppButton class="block">
+        <a href="https://vi.to/hubs/vueforge" target="_blank">Get Started</a>
+      </AppButton>
+    </div>
+  </div>
+</template>
