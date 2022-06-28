b45a9e40dada29fe68a5fd0065644360187431bc
Daniel Kelly <me@danielkelly.io>
2022-06-28 11:28:51 -0500

    provide types

---
+++ src/types/index.ts
@@ -0,0 +1,77 @@
+type ID = string;
+
+export interface Resource8base {
+  id: ID;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+}
+
+export interface Board extends Resource8base {
+  title: string;
+  order: string | Column[];
+
+  // relationships
+  image?: File;
+  tasks?: Task[];
+}
+
+export interface Column {
+  id: ID;
+  title: string;
+  taskUids: ID[];
+}
+
+export interface Task extends Resource8base {
+  title: string;
+  description: string;
+  labels: Label[];
+  dueAt: Date;
+
+  // relationships
+  board?: Board;
+  comments?: Comment[];
+}
+
+export interface Comment extends Resource8base {
+  message: string;
+
+  // relationships
+  task?: Task;
+}
+
+type LabelColor =
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "purple"
+  | "pink";
+
+export interface Label extends Resource8base {
+  label: string;
+  color: LabelColor;
+
+  // relationships
+  board?: Board;
+  tasks?: Task[];
+}
+
+export interface File extends Resource8base {
+  downloadStorageUrl: string;
+  downloadUrl: string;
+  filename: string;
+  meta: {
+    path: string;
+    size: number;
+    mimetype: string;
+    workspaceId: string;
+  };
+  previewUrl: string;
+  provider: string;
+  public: boolean;
+  shareUrl: string;
+  uploadUrl: string;
+  uploaded: boolean;
+}
