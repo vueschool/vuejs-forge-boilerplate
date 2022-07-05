c7645cbcfb02bda8d7d4c9f7132718e12729c625
Daniel Kelly <me@danielkelly.io>
2022-07-05 17:07:33 -0500

    exercise 12 - integrate TwicPics

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
 import { provide } from "vue";
 import { DefaultApolloClient } from "@vue/apollo-composable";
 import apolloClient from "@/graphql/ApolloClient";
+import { useScriptTag } from "@vueuse/core";
 
 // Provide Apollo Client to all Vue Components
 provide(DefaultApolloClient, apolloClient);
+
+// Initialize TwicPic
+useScriptTag(`${import.meta.env.VITE_TWICPICS_URL}?v1`);
 </script>
 
 <template>
--- src/components/AppImage.vue
+++ src/components/AppImage.vue
@@ -1,3 +1,44 @@
+<script setup lang="ts">
+import { toRefs, computed } from "vue";
+const props = defineProps<{
+  src: string;
+}>();
+const { src: image } = toRefs(props);
+
+const appImageBaseURL = "https://cdn.filestackcontent.com/";
+
+// functions for changing a url of image uploaded to 8base
+// to point to twicpics instead
+function toTwicPicsURL(url?: string) {
+  if (!url) return "";
+  return url.replace(appImageBaseURL, "image:");
+}
+function toTwicsPicBlurryImageUrl(url?: string) {
+  if (!url) return "";
+  if (url.startsWith("image:")) {
+    return `${url}?twic=v1/output=preview`;
+  }
+  return url;
+}
+
+const attrs = computed(() => {
+  if (image.value.startsWith("data:")) {
+    return { src: image.value };
+  }
+  const twicPicImage = image.value.startsWith(appImageBaseURL)
+    ? toTwicPicsURL(image.value)
+    : image.value;
+
+  return {
+    "data-twic-src": twicPicImage,
+    src: `${toTwicsPicBlurryImageUrl(image.value)}`,
+  };
+});
+</script>
+
 <template>
-  <img v-bind="$attrs" />
+  <img
+    class="app-image object-cover object-center inline-block"
+    v-bind="attrs"
+  />
 </template>
