53065bbab6deccba8612b19e31ddce7e9bae4c48
Daniel Kelly <me@danielkelly.io>
2022-07-09 14:36:46 -0500

    add global references for vite pages plugin

--- components.d.ts
+++ components.d.ts
@@ -6,26 +6,8 @@ import '@vue/runtime-core'
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AppButton: typeof import('./src/components/AppButton.vue')['default']
-    AppImageDropzone: typeof import('./src/components/AppImageDropzone.vue')['default']
-    AppLoader: typeof import('./src/components/AppLoader.vue')['default']
-    AppPageHeading: typeof import('./src/components/AppPageHeading.vue')['default']
-    BoardCard: typeof import('./src/components/BoardCard.vue')['default']
-    BoardDragAndDrop: typeof import('./src/components/BoardDragAndDrop.vue')['default']
-    BoardMenu: typeof import('./src/components/BoardMenu.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TaskCard: typeof import('./src/components/TaskCard.vue')['default']
-    TaskCreator: typeof import('./src/components/TaskCreator.vue')['default']
-    TheAlerts: typeof import('./src/components/TheAlerts.vue')['default']
-    TheDrawer: typeof import('./src/components/TheDrawer.vue')['default']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
 
