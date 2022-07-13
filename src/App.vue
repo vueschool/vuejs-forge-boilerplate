8c036401bc5619bcd08a2277cfa8e32f337cc84b
Verron Knowles <Verron.Knowles@develme.com>
2022-07-13 12:51:14 -0400

    Final touches on style

--- src/App.vue
+++ src/App.vue
@@ -56,30 +56,32 @@ const toggleTeams = () => {
 </script>
 
 <template>
-  <div class="p-4 border-bottom border-2 flex justify-between" style="margin: -1px">
-    <img src="https://vuejsforge.com/images/logo.svg" width="150" class="max-w-none" alt="Vue.js Forge" />
-    <div>
-      <button ref="button" class="mr-5 text-[#ff6358]" @click="toggleTeams">
-        <span class="k-icon k-i-ungroup"></span>My Team</button>
+  <div class="w-screen h-screen overflow-auto flex flex-col">
+    <div class="p-4 border-bottom border-2 flex justify-between" style="margin: -1px">
+      <img src="https://vuejsforge.com/images/logo.svg" width="150" class="max-w-none" alt="Vue.js Forge" />
+      <div>
+        <button ref="button" class="mr-5 text-[#ff6358]" @click="toggleTeams">
+          <span class="k-icon k-i-ungroup"></span>&nbsp;&nbsp;My Team</button>
 
-      <KPopup anchor="button" :show="showTeamDropdown" class="mt-5 w-48">
-        <ul>
-          <li class="px-5 py-2">Team 1</li>
-          <li class="px-5 py-2">Team 2</li>
-        </ul>
-      </KPopup>
+        <KPopup anchor="button" :show="showTeamDropdown" class="mt-5 w-48">
+          <ul>
+            <li class="px-5 py-2">Team 1</li>
+            <li class="px-5 py-2">Team 2</li>
+          </ul>
+        </KPopup>
 
-      <KButton rounded="full" shape="rectangle" icon="logout" theme-color="warning">Logout</KButton>
+        <KButton rounded="full" shape="rectangle" icon="logout" theme-color="warning">Logout</KButton>
+      </div>
     </div>
+    <Drawer class="flex-1" :expanded="expanded" position="start" mode="push" :mini="true" :items="
+      items.map((item, index) => ({
+        ...item,
+        selected: index === selected,
+      }))
+    " @select="onSelect">
+      <DrawerContent>
+        <RouterView />
+      </DrawerContent>
+    </Drawer>
   </div>
-  <Drawer :expanded="expanded" position="start" mode="push" :mini="true" :items="
-    items.map((item, index) => ({
-      ...item,
-      selected: index === selected,
-    }))
-  " @select="onSelect">
-    <DrawerContent>
-      <RouterView />
-    </DrawerContent>
-  </Drawer>
 </template>
