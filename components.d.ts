149e0078c3663cb30f917ec9cfe1cb6a190d44f6
diegocastro00 <diego@paytient.com>
2022-07-13 23:14:11 -0300

    VF-Trello: Board Widget Component

--- components.d.ts
+++ components.d.ts
@@ -7,13 +7,14 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AppButton: typeof import('./src/components/AppButton.vue')['default']
     AppLayout: typeof import('./src/components/AppLayout.vue')['default']
-    CustomHeader: typeof import('./src/components/layout/custom-header.vue')['default']
-    Header: typeof import('./src/components/layout/Header.vue')['default']
-    Layout: typeof import('./src/components/Layout.vue')['default']
-    Navbar: typeof import('./src/components/layout/Navbar.vue')['default']
+    BoardCard: typeof import('./src/components/BoardCard.vue')['default']
+    BoardWidget: typeof import('./src/components/BoardWidget.vue')['default']
+    CustomHeader: typeof import("./src/components/layout/custom-header.vue")["default"];
+    Header: typeof import("./src/components/layout/Header.vue")["default"];
+    Layout: typeof import("./src/components/Layout.vue")["default"];
+    Navbar: typeof import("./src/components/layout/Navbar.vue")["default"];
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Undefined: typeof import('./src/components/index.vue')['default']
   }
 }
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const boards = [
+  {
+    title: "My Awesome Project",
+    image: {
+      downloadUrl: "https://picsum.photos/200/300",
+    },
+    id: "1",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: new Date(),
+    order: "",
+  },
+  {
+    title: "My New Project",
+    image: {
+      downloadUrl: "https://picsum.photos/200/300",
+    },
+    id: "2",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: new Date(),
+    order: "",
+  },
+  {
+    title: "Trello clone",
+    image: {
+      downloadUrl: "https://picsum.photos/200/300",
+    },
+    id: "3",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: new Date(),
+    order: "",
+  },
+];
+</script>
 <template>
-  <div class="flex justify-center items-center h-[80vh]">
-    <div class="text-center">
-      <img
-        class="block"
-        src="https://vuejsforge.com/images/logo.svg"
-        alt="Vue.js Forge"
-      />
-      <AppButton class="block">
-        <a href="https://vi.to/hubs/vuejs-forge/" target="_blank"
-          >Get Started</a
-        >
-      </AppButton>
-    </div>
+  <div class="mx-16 my-16 flex w-full">
+    <BoardCard v-for="(board, index) in boards" :key="index" :board="board" />
   </div>
 </template>
