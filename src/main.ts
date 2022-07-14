e586c2a421e945d11cc3e9e79a08eb1e95cb3a32
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-14 09:30:31 -0600

    changed css naming conventions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "@/assets/base.css";
-import "@progress/kendo-theme-default/dist/all.css";
+import "@/assets/tailwind.css";
 
 const app = createApp(App);
 
