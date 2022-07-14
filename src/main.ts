7d1ee0a212d4bb9ecc787b4c53fd4594af2746de
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-14 09:48:57 -0600

    finished adding base inkline to project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { Inkline, components } from "@inkline/inkline";
+
+import "@/assets/inkline.scss"
 import "@/assets/tailwind.css";
 
 const app = createApp(App);
+
+app.use(Inkline, { 
+  components
+});
 
 app.use(router);
 
