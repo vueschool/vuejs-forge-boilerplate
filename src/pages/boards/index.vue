0b200498b041b77a21b2796277efd7cb797b10a5
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-14 09:55:17 -0600

    added static board content to boards index page

--- src/pages/boards/index.vue
+++ src/pages/boards/index.vue
@@ -1,3 +1,38 @@
-<script setup>
+<script setup lang="ts">
+import type { Board } from "@/types";
+
+const staticBoards = [
+  {
+    id: "1",
+    title: "The First Board",
+    // array wrapped in a string will be compiled to just an array - typescript thing
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?board=1"
+    }
+  },
+  {
+    id: "2",
+    title: "The Second Board",
+    // array wrapped in a string will be compiled to just an array - typescript thing
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?board=2"
+    }
+  },
+  {
+    id: "3",
+    title: "The Third Board",
+    // array wrapped in a string will be compiled to just an array - typescript thing
+    order: "[]",
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?board=3"
+    }
+  }
+]
 </script>
-<template></template>
+<template>
+<main>
+
+</main>
+</template>
